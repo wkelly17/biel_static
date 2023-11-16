38d83d1624ec466548be1ef35ea3006f352e42ec
Will Kelly <willkelly9617@gmail.com>
2023-11-16 10:02:37 -0600

    work on action

--- translate/src/index.ts
+++ translate/src/index.ts
@@ -1,20 +1,26 @@
 import fs, {writeFileSync} from "node:fs";
 import {readdir, stat} from "fs/promises";
-import locales from "../../site/src/config/locales.json";
+// import locales from "../../site/src/config/locales.json";
 import {getInput} from "@actions/core";
-
+const rootDir = process.cwd();
+const localesPath = `${rootDir}/site/src/config/locales.json`;
+const localesString = fs.readFileSync(localesPath, {
+  encoding: "utf-8",
+});
+const localesJson = JSON.parse(localesString);
+console.log({localesJson});
 const name = getInput("name");
+console.log({name});
 
 // console.log({name});
 // locales.forEach((locale) => {
 //   console.log({locale});
 // });
-let rootDir = process.cwd();
-console.log({rootDir});
-let text_file_location = rootDir + "/site/README.md";
+// console.log({rootDir});
+// let text_file_location = rootDir + "/site/README.md";
 
-let data = fs.readFileSync(text_file_location, {
-  encoding: "utf-8",
-});
+// let data = fs.readFileSync(text_file_location, {
+//   encoding: "utf-8",
+// });
 
-console.log({data});
+// console.log({data});
